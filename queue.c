db05e9727d45f5b3ff874c264d5fa92475492c27
David Moon <dm7@williams.edu>
2016-02-25 23:41:51 -0500

    WORKING QUEUEgit status

--- queue.c
+++ queue.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
+#include <errno.h>
 #include "queue.h"
 #include "util.h"
 
@@ -58,38 +59,43 @@ void enqueue(queue_t *q, int fd) {
   pthread_mutex_lock(q->tlock);
   q->tail->next = node;
   q->tail = node;
-  DEBUG_PRINT("Enqueued node %p { fd = %d }\n",node,fd);
-  pthread_mutex_unlock(q->tlock);
+  DEBUG_PRINT("\t\t\t\t\t| enqueued %p { fd = %d }\n\t\t\t\t\t|    dummy %p { fd = %d }\n",node,fd,q->head,q->head->fd);
   pthread_cond_signal(q->nonempty);
+  pthread_mutex_unlock(q->tlock);
 }
 
 void dequeue(queue_t *q, int *fd) {
   node_t *dummy, *new_dummy;
   pthread_mutex_lock(q->hlock);
-  dummy = q->head;
-  new_dummy = dummy->next;
+  //dummy = q->head;
+  //new_dummy = dummy->next;
   /*
   if (new_head == NULL) {
     pthread_mutex_unlock(q->hlock);
     return -1;
   }
   */
-  DEBUG_PRINT("dequeue %d: before while\n", ((int)fd)%1000);  
-  while (new_dummy == NULL) {
-    DEBUG_PRINT("dequeue %d: inside while\n", ((int)fd)%1000);
+  DEBUG_PRINT("[dq %p] before loop (%p,%p)\n", fd,dummy,new_dummy);//((int)fd)%1000);  
+  while (q->head->next == NULL) {
+    //    DEBUG_PRINT("[dq %p] inside loop, dummy: node %p { fd = %d }\n", fd, q->head, q->head->fd);//((int)fd)%1000);
     pthread_cond_wait(q->nonempty, q->hlock);
 
-    dummy = q->head;
-    new_dummy = dummy->next;
+    //dummy = q->head;
+    //new_dummy = dummy->next;
+    //DEBUG_PRINT("[dq %p] inside loop (%p,%p)\n", fd,dummy,new_dummy);
   }
-  DEBUG_PRINT("dequeue %d: after while\n", ((int)fd)%1000);
-  DEBUG_PRINT("About to dequeue node %p { fd = %d }\n",new_dummy,new_dummy->fd);
+  //DEBUG_PRINT("[dq %p] after while\n", fd);//((int)fd)%1000);
+  dummy = q->head;
+  new_dummy = dummy->next;
+  DEBUG_PRINT("[dq %p] ready    %p { fd = %d }\n",fd,new_dummy,new_dummy->fd);
   
   *fd = new_dummy->fd;
+  q->head = new_dummy;
   pthread_mutex_unlock(q->hlock);
-  DEBUG_PRINT("dequeue %d: before free\n", ((int)fd)%1000);
+  //DEBUG_PRINT("[dq %p] before free\n", fd);//((int)fd)%1000);
   free(dummy);
-  DEBUG_PRINT("dequeue %d: after free\n", ((int)fd)%1000);
+  //DEBUG_PRINT("[dq %p] after free\n", fd);//((int)fd)%1000);
+  DEBUG_PRINT("[dq %p] dequeued %p { fd = %d }\n",fd,new_dummy,new_dummy->fd);
   //return 0;
 }
 
@@ -102,7 +108,7 @@ void *test_enqueue(void *arg) {
   //pthread_detach(pthread_self());
   iq_pair *iq = (iq_pair *)arg;
   enqueue(iq->q, iq->i);
-  DEBUG_PRINT("Enqueued %d\n",iq->i);
+  //  DEBUG_PRINT("Enqueued %d\n",iq->i);
   return NULL;
 }
 
@@ -110,7 +116,8 @@ void *test_dequeue(void *arg) {
   queue_t *q = (queue_t *)arg;
   int fd;
   dequeue(q,&fd);
-  DEBUG_PRINT("Dequeued %d (%d)\n",fd,((int)&fd)%1000);
+  //  DEBUG_PRINT("Dequeued %d (%d)\n",fd,((int)&fd)%1000);
+  printf("[dq %p] about to return\n",&fd);
   return NULL;
 }
 
@@ -130,7 +137,7 @@ int main() {
     iqs[j]->i = j;  iqs[j]->q = q;
   }
   for (j = 0; j < n; j++) {
-    pthread_create(&deq_tids[n], NULL, test_dequeue, (void *)q);
+    pthread_create(&deq_tids[j], NULL, test_dequeue, (void *)q);
   }  
   for (j = 0; j < n; j++) {
     pthread_create(&enq_tids[j], NULL, test_enqueue, (void *)iqs[j]);
@@ -138,10 +145,35 @@ int main() {
   }
 
   int *ret;
+  int e;
   for (j = 0; j < n; j++) {
-    pthread_join(enq_tids[j], (void *)&ret);
-    free(iqs[j]);
+    e = pthread_join(enq_tids[j], (void *)&ret);
+    if (e == EDEADLK)
+      printf("enq thread %d is deadlocked!\n",j);
+    else if (e == EINVAL)
+      printf("enq thread %d is not joinable!\n",j);
+    else if (e == ESRCH)
+      printf("enq thread %d could not be found!\n",j);
+    else if (e == 0)
+      printf("enq thread %d was joined successfully!\n",j);
+    else
+      printf("enq thread %d is ???\n",j);
   }
+  for (j = 0; j < n; j++) {
+    e = pthread_join(deq_tids[j], (void *)&ret);
+    if (e == EDEADLK)
+      printf("deq thread %d is deadlocked!\n",j);
+    else if (e == EINVAL)
+      printf("deq thread %d is not joinable!\n",j);
+    else if (e == ESRCH)
+      printf("deq thread %d could not be found!\n",j);
+    else if (e == 0)
+      printf("deq thread %d was joined successfully!\n",j);
+    else
+      printf("deq thread %d is ???\n",j);
+  }
+  for (j = 0; j < n; j++) free(iqs[j]);
 
+  
   queue_destroy(q);
 }
